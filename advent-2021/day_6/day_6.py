0ac8e7b0dab3172b9219dfc7fc33c02e309c23ee
sergiokv13 <sergiokv13@gmail.com>
2021-12-06 11:39:37 -0400

    day 6 with arr

--- advent-2021/day_6/day_6.py
+++ advent-2021/day_6/day_6.py
@@ -1,34 +1,30 @@
 import fileinput
 
-lanterfish = {}
-
-def inc_dict(my_dict, el, val):
-  if my_dict.get(el): my_dict[el] += val
-  else: my_dict[el] = val
+lanterfish = [0] * 10
 
 for line in fileinput.input():
   elems = [int(n) for n in line.split(',')]
 for el in elems:
-  inc_dict(lanterfish, el, 1)
+ lanterfish[el] += 1
 
 def simulate(days, lanterfish):
   for _i in range(days):
-    if (lanterfish.get(0)):
-      inc_dict(lanterfish, 9, lanterfish.get(0))
-      inc_dict(lanterfish, 7, lanterfish.get(0))
+    if (lanterfish[0]):
+      lanterfish[9] += lanterfish[0]
+      lanterfish[7] += lanterfish[0]
       lanterfish[0] = 0
 
     l_copy = lanterfish.copy()
-    for day, count in l_copy.items():
+    for day, count in enumerate(l_copy):
       if (count and day):
-        inc_dict(lanterfish, day, count * -1)
-        inc_dict(lanterfish, day-1, count)
+        lanterfish[day] -= count 
+        lanterfish[day-1] += count
     
-  return sum([c for _k,c in lanterfish.items()])
+  return sum(lanterfish)
 
 # # First Star
 print(simulate(80, lanterfish))
 
-# # Second Star
+# # # Second Star
 
 print(simulate(256, lanterfish))
